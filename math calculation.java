890817d5423a736088daf16b2ad2b7512cc43777
renshiya <43057327+renshiya@users.noreply.github.com>
2018-09-07 12:33:23 +0530

    Add files via upload

---
+++ math calculation.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int a;
+    float b,c,d,e,f;
+    a=5;
+    b=8;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+  System.out.println("the sum of two number is"+c);
+  System.out.println("the diffraction of two number is"+d);
+  System.out.println("the product of two number is"+e);
+  System.out.println("the division of two number is"+f);
+ }
+}
