561e1843586031b33fa9f6595694b87103e5c387
renshiya <43057327+renshiya@users.noreply.github.com>
2018-10-06 12:31:27 +0530

    Add files via upload

---
+++ positive.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double number = 12.3;
+        if (number < 0.0)
+            System.out.println(number + " is a negative number.");
+        else if ( number > 0.0)
+            System.out.println(number + " is a positive number.");
+        else
+            System.out.println(number + " is 0.");
+    }
+}
